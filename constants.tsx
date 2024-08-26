0fdd9ddd7db3f193c38d42d4a8327a977aac2b80
MuhidinM <misbahmuhidin3@gmail.com>
2024-08-26 10:14:59 +0300

    detail desc

--- constants.tsx
+++ constants.tsx
@@ -7,28 +7,44 @@ export const products: Product[] = [
     description: (
       <>
         <p className="mb-2">
-          The key to overcoming poverty lies in the power of cooperation and
-          cooperatives&quot;, as envisioned by Obbo Haile Gebre Lube, the
-          founding father of CoopBank. He recognized the struggles and neglect
-          faced by rural and underprivileged communities.
+          Coop Stream is CoopBank&apos;s innovative loan origination app that
+          automates the entire loan processing workflow. This app simplifies and
+          accelerates the process of applying for and approving loans, reducing
+          manual tasks and enhancing efficiency.
         </p>
         <p>
-          Inspired by his vision, we believe that a better tomorrow for our
-          community rests in the hands of young innovators from these very
-          communities. That&apos;s why we are committed to cooperating with them
-          to transform lives and create lasting change.
+          By streamlining loan origination, Coop Stream allows both customers
+          and bank staff to manage loan applications with ease. The app ensures
+          quicker decision-making, improves accuracy, and delivers a seamless
+          experience from application to approval, reflecting CoopBank&apos;s
+          commitment to leveraging technology for better financial services.
         </p>
       </>
     ),
     link: "",
-    file: "presentation.pptx",
+    file: "",
     moto: "",
     imageUrl: "/products/coop-stream.png",
   },
   {
     id: 2,
     name: "Debbo",
-    description: "",
+    description: (
+      <>
+        <p className="mb-2">
+          The Deboo Crowdfunding System is a web-based platform offered by
+          Cooperative Bank of Oromia. It enables individuals and organizations
+          to create fundraising campaigns and receive contributions from a large
+          number of people to support their projects, causes, or initiatives.
+        </p>
+        <p>
+          The Deboo Crowdfunding System supports various types of campaigns,
+          including personal projects, creative endeavors, social causes,
+          entrepreneurial ventures, community initiatives, and more. You can
+          create campaigns to raise funds for a wide range of purposes.
+        </p>
+      </>
+    ),
     link: "https://debo.coopbankoromiasc.com/",
     file: "",
     moto: "",
@@ -37,7 +53,19 @@ export const products: Product[] = [
   {
     id: 3,
     name: "Diaspora",
-    description: "",
+    description: (
+      <>
+        <p className="mb-2">
+          Diaspora Banking Accounts allow Diasporas who resides and works
+          outside the country to maintain and perform domestic and international
+          transfers through their Coopbank Diaspora Accounts.
+        </p>
+        <p>
+          The bank provides business and investment loan along with expertise
+          free consultancy services on different opportunities.
+        </p>
+      </>
+    ),
     link: "https://diasporabanking.coopbankoromiasc.com/",
     file: "",
     moto: "",
@@ -46,8 +74,25 @@ export const products: Product[] = [
   {
     id: 4,
     name: "Michu",
-    description: "",
-    link: "",
+    description: (
+      <>
+        <p className="mb-2">
+          Michu is CoopBank&apos;s groundbreaking digital lending platform, one
+          of the first in Ethiopia to offer loans without requiring collateral.
+          This innovative service provides quick and accessible financing
+          options to customers, making it easier for individuals and businesses
+          to meet their financial needs.
+        </p>
+        <p>
+          As a non-collateral lending platform, Michu leverages digital
+          technology to assess creditworthiness and disburse loans swiftly. It
+          opens up new opportunities for those who may not have traditional
+          assets, empowering more people across Ethiopia to access credit and
+          support their financial goals.
+        </p>
+      </>
+    ),
+    link: "https://michu.coopbankoromiasc.com",
     file: "",
     moto: "",
     imageUrl: "/products/MICHU.png",
@@ -55,16 +100,48 @@ export const products: Product[] = [
   {
     id: 5,
     name: "Souq Pass",
-    description: "",
+    description: (
+      <>
+        <p className="mb-2">
+          Souqpass is a financial platform powered by Cooperative Bank of
+          Oromia, dedicated to providing innovative financial solutions tailored
+          to meet the evolving needs of businesses and individuals.
+        </p>
+
+        <p className="">
+          Souqpass offers Revenue-Based Financing (RBF) designed to support
+          businesses&apos; growth aspirations. RBF aligns with your
+          business&apos;s revenue stream, providing flexible funding solutions
+          that adapt to your income flow.
+        </p>
+      </>
+    ),
     link: "https://souqpass.coopbankoromiasc.com/",
-    file: "presentation.pptx",
+    file: "",
     moto: "",
     imageUrl: "/products/souqpass.png",
   },
   {
     id: 6,
     name: "Coop Recon",
-    description: "",
+    description: (
+      <>
+        <p className="mb-2">
+          Coop Recon is a powerful tool designed by CoopBank to streamline
+          reconciliation processes both within the bank and with external banks
+          and systems. It automates the matching of transactions, ensuring that
+          all accounts are accurately balanced and discrepancies are quickly
+          identified.
+        </p>
+        <p>
+          By integrating with various systems, Coop Recon simplifies the complex
+          process of reconciliation, reducing manual errors and improving
+          efficiency. This product enhances the bank&apos;s ability to maintain
+          financial accuracy and fosters smoother interactions with external
+          banking partners.
+        </p>
+      </>
+    ),
     link: "",
     file: "",
     moto: "",
@@ -73,7 +150,21 @@ export const products: Product[] = [
   {
     id: 7,
     name: "Equb",
-    description: "",
+    description: (
+      <>
+        <p className="mb-2">
+          The Equb app by CoopBank digitizes the traditional Ethiopian practice
+          of rotating savings and credit. It allows users to easily join or
+          create Equb groups, contributing and receiving funds securely through
+          their smartphones.
+        </p>
+        <p>
+          This app modernizes a cultural tradition, making it more accessible
+          and convenient. Users can manage their Equb participation seamlessly,
+          with notifications and tracking features enhancing the experience.
+        </p>
+      </>
+    ),
     link: "",
     file: "",
     moto: "",
@@ -82,7 +173,25 @@ export const products: Product[] = [
   {
     id: 8,
     name: "Coop Ambition",
-    description: "",
+    description: (
+      <>
+        <p className="mb-2">
+          Coop Ambition is CoopBank&apos;s dedicated e-learning platform,
+          designed to provide employees with access to a wide range of
+          educational resources. This platform supports continuous learning and
+          professional development, enabling staff to enhance their skills and
+          knowledge at their own pace.
+        </p>
+        <p>
+          Through Coop Ambition, employees can engage with interactive courses,
+          training modules, and other educational content tailored to their
+          roles within the bank. This initiative underscores CoopBank&apos;s
+          commitment to fostering a culture of growth and excellence, ensuring
+          that its workforce remains equipped to meet the evolving demands of
+          the banking industry.
+        </p>
+      </>
+    ),
     link: "http://learn.coopbank.local/",
     file: "",
     moto: "",
@@ -91,7 +200,15 @@ export const products: Product[] = [
   {
     id: 9,
     name: "VSLA",
-    description: "",
+    description: (
+      <>
+        <p className="mb-2">
+          Village Savings and Loans Associations (VSLAs) empower participants to
+          increase access and control over resources and use collective power to
+          overcome social and financial barriers.
+        </p>
+      </>
+    ),
     link: "",
     file: "",
     moto: "",
@@ -100,7 +217,22 @@ export const products: Product[] = [
   {
     id: 10,
     name: "Coop App",
-    description: "",
+    description: (
+      <>
+        <p className="mb-2">
+          The Mobile Banking Application for CoopBank offers customers a
+          convenient way to manage their finances on the go. With features like
+          account management, fund transfers, and bill payments, users can
+          handle their banking needs directly from their smartphones.
+        </p>
+        <p>
+          Designed with security in mind, the app provides robust protection
+          through biometric login and real-time notifications. CoopBank ensures
+          that customers have a reliable and easy-to-use platform to access
+          their accounts anytime, anywhere.
+        </p>
+      </>
+    ),
     link: "",
     file: "",
     moto: "",
